5036743951904aef6df4f492cab9f76cb5a74af8
amaelDago <amaeldago@gmail.com>
2021-04-29 09:01:00 +0200

    Add config

---
+++ config.py
@@ -0,0 +1,17 @@
+# Dictionary of coef
+
+coef_dict = {
+    "doi" : 1, 
+    "doc_type" : 1,
+    "default_title" : 1,
+    "meeting" : 1,
+    "journal" : 1,
+    "settlement" :1,
+    "source" : 1,
+    "page_range" :1,
+    "issue" : 1,
+    "nnt" : 1,
+    "issn" : 1,
+    "eissn" : 1,
+    "publi_date" : 1
+}
